8765cdfd14646f2891403af5e19bba80d100a036
soregashi-27 <tomo.soul.15@gmail.com>
2021-07-27 00:25:36 +0900

    vuex bindingsでjsonfileを画面上に表示

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,26 +1,28 @@
 //Send to request for back-end
-// import axios from 'axios';
+import axios from 'axios';
 
 const state = {
-  todos: [
-    {
-      id: 1,
-      title: 'Todo one',
-    },
-    {
-      id: 2,
-      title: 'Todo Two',
-    },
-  ],
+  todos: [],
 };
 
 const getters = {
   allTodos: (state) => state.todos,
 };
 
-const actions = {};
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos'
+    );
 
-const mutations = {};
+    // console.log(response.data);
+    commit('setTodos', response.data);
+  },
+};
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos),
+};
 
 export default {
   state,
